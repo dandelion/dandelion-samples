e76d6b00afeffdecc3f165fb306bc9bbb6005695
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-11-24 23:19:17 +0100

    Added new datatables-thymeleaf-basics sample

---
+++ datatables/datatables-thymeleaf-basics/README.md
@@ -0,0 +1,43 @@
+datatables-thymeleaf-basics
+=================================================================
+
+A sample which shows the basics of Dandelion-Datatables when using Thymeleaf: feature enablement, sorting, filtering, paging and more. See the full list below.
+
+## Technical stack
+
+ - Thymeleaf 2.1.1.RELEASE
+ - Dandelion-Datatables 1.10.0
+ - Spring 3.2.5.RELEASE
+
+## Features
+		
+ - Data source type: DOM
+ - Feature enablement
+ - Sorting
+ - Filtering
+ - Paging
+ - Scrolling
+ - Column rendering
+ - DOM positioning
+ - Multiple tables
+ - Basic styling
+ - AssetServlet (lien doc)
+
+## Running this sample
+
+Using __Apache Tomcat__ :
+
+    mvn tomcat7:run
+
+Using __Jetty__:
+
+    mvn jetty:run
+
+You can then access the sample here: [http://localhost:9090/datatables-thymeleaf-basics](http://localhost:9090/datatables-thymeleaf-basics)
+
+## Bug/improvement
+
+Please report it using the general issue tracker: [https://github.com/dandelion/issues/issues](https://github.com/dandelion/issues/issues)
+
+=
+The [Dandelion team](http://dandelion.github.io/team/).
---
+++ datatables/datatables-thymeleaf-basics/src/main/java/com/github/dandelion/datatables/web/SampleController.java
@@ -0,0 +1,85 @@
+package com.github.dandelion.datatables.web;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.github.dandelion.datatables.model.Person;
+import com.github.dandelion.datatables.service.PersonService;
+
+/**
+ * Controllers for the all demo examples.
+ * 
+ * @author Thibault Duchateau
+ */
+@Controller
+@RequestMapping(method = RequestMethod.GET)
+public class SampleController {
+
+	@Autowired
+	private PersonService personService;
+
+	/**
+	 * <p>
+	 * Populates the model with the domain objects. Used in all examples that
+	 * use a DOM source.
+	 * 
+	 * @return a list of persons for display.
+	 */
+	@ModelAttribute("persons")
+	public List<Person> populateTable() {
+		return personService.findLimited(100);
+	}
+
+	@RequestMapping(value = "/")
+	public String goToIndex(HttpServletRequest request) {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/basics/sorting/{page}")
+	public String goToSortingExample(@PathVariable String page) {
+		return "basics/sorting/" + page;
+	}
+	
+	@RequestMapping(value = "/basics/filtering/{page}")
+	public String goToFilteringExample(@PathVariable String page) {
+		return "basics/filtering/" + page;
+	}
+	
+	@RequestMapping(value = "/basics/paging/{page}")
+	public String goToPagingExample(@PathVariable String page) {
+		return "basics/paging/" + page;
+	}
+	
+	@RequestMapping(value = "/basics/scrolling/{page}")
+	public String goToScrollingExample(@PathVariable String page) {
+		return "basics/scrolling/" + page;
+	}
+	
+	@RequestMapping(value = "/column-rendering/{page}")
+	public String goToColumnRenderingExample(@PathVariable String page) {
+		return "column-rendering/" + page;
+	}
+	
+	@RequestMapping(value = "/multiple-tables")
+	public String goToMultipleTablesExample() {
+		return "multiple-tables";
+	}
+	
+	@RequestMapping(value = "/dom-positioning")
+	public String goToDomPositioningExample() {
+		return "dom-positioning";
+	}
+	
+	@RequestMapping(value = "/styling")
+	public String goToStylingExample() {
+		return "styling";
+	}
+}
