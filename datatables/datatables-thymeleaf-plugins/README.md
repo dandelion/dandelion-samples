4b58e0cd77ae978630b0c09105ecbe5061c41c1a
Thibault Duchateau <thibault.duchateau@gmail.com>
2014-01-04 23:07:31 +0100

    Added datatables-thymeleaf-plugins

---
+++ datatables/datatables-thymeleaf-plugins/README.md
@@ -0,0 +1,36 @@
+datatables-thymeleaf-plugins
+=================================================================
+
+A sample which shows how to use with Thymeleaf the DataTables' extras supported by Dandelion-Datatables, such as [Scroller](http://datatables.net/extras/scroller/) or [ColReorder](http://datatables.net/extras/colreorder/).
+
+## Technology stack
+
+ - Thymeleaf 2.1.1.RELEASE
+ - Dandelion-Datatables 0.10.0
+ - Spring 3.2.5.RELEASE
+
+## Features
+		
+ - __Data source type__: DOM
+ - Scroller plugin - <a href="http://dandelion.github.io/datatables/tutorials/plugins/scroller.html">Doc</a>
+ - ColReorder plugin - <a href="http://dandelion.github.io/datatables/tutorials/plugins/colreorder.html">Doc</a>
+ - FixedHeader plugin - <a href="http://dandelion.github.io/datatables/tutorials/plugins/fixedheader.html">Doc</a>
+
+## Running this sample
+
+Using __Apache Tomcat__ :
+
+    mvn tomcat7:run
+
+Using __Jetty__:
+
+    mvn jetty:run
+
+You can then access the sample here: [http://localhost:9090/datatables-thymeleaf-plugins](http://localhost:9090/datatables-thymeleaf-plugins)
+
+## Bug/improvement
+
+Please report it using the general issue tracker: [https://github.com/dandelion/issues/issues](https://github.com/dandelion/issues/issues)
+
+=
+The [Dandelion team](http://dandelion.github.io/team/).
---
+++ datatables/datatables-thymeleaf-plugins/src/main/java/com/github/dandelion/datatables/web/SampleController.java
@@ -0,0 +1,61 @@
+package com.github.dandelion.datatables.web;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.github.dandelion.datatables.model.Person;
+import com.github.dandelion.datatables.service.PersonService;
+
+/**
+ * <p>
+ * Controller for all examples contained in the sample.
+ * 
+ * @author Thibault Duchateau
+ */
+@Controller
+@RequestMapping(method = RequestMethod.GET)
+public class SampleController {
+
+	@Autowired
+	private PersonService personService;
+
+	/**
+	 * <p>
+	 * Populates the model with the domain objects. Used in all examples that
+	 * use a DOM source.
+	 * 
+	 * @return a list of persons for display.
+	 */
+	@ModelAttribute("persons")
+	public List<Person> populateTable() {
+		return personService.findLimited(100);
+	}
+
+	@RequestMapping(value = "/")
+	public String goToIndex(HttpServletRequest request) {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/colreorder/{page}")
+	public String goToColreorderExample(@PathVariable String page) {
+		return "colreorder/" + page;
+	}
+	
+	@RequestMapping(value = "/scroller/{page}")
+	public String goToScrollerExample(@PathVariable String page) {
+		return "scroller/" + page;
+	}
+	
+	@RequestMapping(value = "/fixedheader/{page}")
+	public String goToFixedhederExample(@PathVariable String page) {
+		return "fixedheader/" + page;
+	}
+}
