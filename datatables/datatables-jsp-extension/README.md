066f0ce9adb3a8d83317bc5e07de82dc799a147c
Thibault Duchateau <thibault.duchateau@gmail.com>
2014-01-16 09:32:15 +0100

    Added new JSP sample demonstrating the extension mechanism

---
+++ datatables/datatables-jsp-extension/README.md
@@ -0,0 +1,36 @@
+datatables-jsp-extension
+=================================================================
+
+A sample which show how to use the extension mechanism.
+
+## Technology stack
+
+ - JSP 
+ - JSTL 1.2
+ - Apache Tiles 2.2.2
+ - Dandelion-Datatables 0.10.0
+ - Spring 3.2.5.RELEASE
+
+## Features
+		
+ - __Data source type__: DOM
+ - Activating a custom extension
+
+## Running this sample
+
+Using __Apache Tomcat__ :
+
+    mvn tomcat7:run
+
+Using __Jetty__:
+
+    mvn jetty:run
+
+You can then access the sample here: [http://localhost:9090/datatables-jsp-extension](http://localhost:9090/datatables-jsp-extension)
+
+## Bug/improvement
+
+Please report it using the general issue tracker: [https://github.com/dandelion/issues/issues](https://github.com/dandelion/issues/issues)
+
+=
+The [Dandelion team](http://dandelion.github.io/team/).
---
+++ datatables/datatables-jsp-extension/src/main/java/com/github/dandelion/datatables/web/SampleController.java
@@ -0,0 +1,51 @@
+package com.github.dandelion.datatables.web;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.github.dandelion.datatables.model.Person;
+import com.github.dandelion.datatables.service.PersonService;
+
+/**
+ * <p>
+ * Controller for all examples contained in the sample.
+ * 
+ * @author Thibault Duchateau
+ */
+@Controller
+@RequestMapping(method = RequestMethod.GET)
+public class SampleController {
+
+	@Autowired
+	private PersonService personService;
+
+	/**
+	 * <p>
+	 * Populates the model with the domain objects. Used in all examples that
+	 * use a DOM source.
+	 * 
+	 * @return a list of persons for display.
+	 */
+	@ModelAttribute("persons")
+	public List<Person> populateTable() {
+		return personService.findLimited(100);
+	}
+
+	@RequestMapping(value = "/")
+	public String goToIndex(HttpServletRequest request) {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/{page}")
+	public String goToPage(@PathVariable String page) {
+		return page;
+	}
+}
