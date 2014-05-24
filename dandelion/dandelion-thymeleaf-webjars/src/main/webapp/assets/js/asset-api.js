f052cf986809daeee4151a0310d180e29365cef1
Thibault Duchateau <thibault.duchateau@gmail.com>
2014-05-25 01:32:55 +0200

    Removed old samples

--- dandelion/dandelion-thymeleaf-webjars/src/main/webapp/assets/js/asset-api.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('#msg')
-		.html('<h3>Hey! This awesome animation comes from the asset loaded by the bundle "bundle-using-api", which has been programmatically activated</h3>')
-		.fadeIn(1000)
-		.fadeOut(1000)
-		.fadeIn(1000)
-		.fadeOut(1000)
-		.fadeIn(1000);
-});
